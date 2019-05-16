91b23afd9f2e35c8b0d6ae03178042e3d1a65f90
Max Base <MaxBaseCode@Gmail.Com>
2019-05-17 00:19:59 +0430

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
-
+/**
+*
+* @Name : TinyCalculator/main.cpp
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-05-17
+* @Released under : https://github.com/BaseMax/TinyCalculator/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/TinyCalculator
+*
+**/
