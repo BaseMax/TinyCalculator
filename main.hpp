4f75109fb2bd056abb5293ff58db8bd59c47f226
Max Base <MaxBaseCode@Gmail.Com>
2019-05-17 00:20:15 +0430

    Update main.hpp

--- main.hpp
+++ main.hpp
@@ -8,3 +8,23 @@
 * @Repository : https://github.com/BaseMax/TinyCalculator
 *
 **/
+#ifndef MAIN_H
+	#define MAIN_H
+
+	#include <iostream>
+	#include <cmath>
+	using namespace std;
+
+	void parseAddition();
+	void parseSubtraction();
+	void parseDivision();
+	void parseMultiplication();
+	void parseExponentiation();
+	void parseSquareRoot();
+	void parseSine();
+	void parseCosine();
+	void parseTangent();
+	void help();
+	void error();
+
+#endif
