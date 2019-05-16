4b33bf663a20661965870921f86b92c25b19ff04
Max Base <MaxBaseCode@Gmail.Com>
2019-05-17 00:22:18 +0430

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,90 @@
 * @Repository : https://github.com/BaseMax/TinyCalculator
 *
 **/
-#include "main.h"
+#include "main.hpp"
+
+void parseAddition() {
+	float a=0, b=0;
+	cin >> a;
+	cin >> b;
+	cout << a + b << endl;
+}
+
+void parseSubtraction() {
+	float a=0, b=0;
+	cin >> a;
+	cin >> b;
+	cout << a - b << endl;
+}
+
+
+void parseMultiplication() {
+	float a=0, b=0;
+	cin >> a;
+	cin >> b;
+	cout << a * b << endl;
+}
+
+void parseDivision() {
+	float a=0, b=0;
+	cin >> a;
+	cin >> b;
+	if(b != 0) {
+		cout << a / b << endl;
+	}
+}
+
+void parseExponentiation() {
+	float a=0, r=0;
+	int b=0;
+	cin >> a;
+	cin >> b;
+	r=pow(a, b);
+	cout << r << endl;
+}
+
+void parseSquareRoot() {
+}
+
+void parseSine() {
+	float a=0, r=0;
+	cin >> a;
+	r=sin(a);
+	cout << r << endl;
+}
+
+void parseCosine() {
+	float a=0, r=0;
+	cin >> a;
+	r=cos(a);
+	cout << r << endl;
+}
+
+void parseTangent() {
+	float a=0, r=0;
+	cin >> a;
+	r=tan(a);
+	cout << r << endl;
+}
+
+void help() {
+	cout << "\n\n";
+	cout << " Tiny Calculator ------------------------------\n";
+	cout << "\t c\t\t add two number\n";
+	cout << "\t c\t\t add two number\n";
+	cout << "\t c\t\t add two number\n";
+	cout << "\t c\t\t add two number\n";
+	cout << "\t c\t\t add two number\n";
+	cout << "\t c\t\t add two number\n";
+	cout << "\t c\t\t add two number\n";
+	cout << "\t c\t\t add two number\n";
+	cout << "\t c\t\t add two number\n";
+	cout << "\n\n";
+}
+
+void error() {
+	cout << "Error!\n";
+}
 
 int main(int argc, char** argv) {
 	unsigned char type;
