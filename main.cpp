237eff3ade05a304b0c1b3e3a9301eb47cb97f8c
Max Base <MaxBaseCode@Gmail.Com>
2019-05-17 00:21:16 +0430

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,3 +9,51 @@
 *
 **/
 #include "main.h"
+
+int main(int argc, char** argv) {
+	unsigned char type;
+	bool flag=true;
+	while(flag) {
+		cout << "Enter Your type: ";
+		cin >> type;
+		switch(type) {
+			case 'a':
+				parseAddition();
+			break;
+			case 'm':
+				parseSubtraction();
+			break;
+			case 'u':
+				parseMultiplication();
+			break;
+			case 'd':
+				parseDivision();
+			break;
+			case 'p':
+				parseExponentiation();
+			break;
+			case 'r':
+				parseSquareRoot();
+			break;
+			case 's':
+				parseSine();
+			break;
+			case 'c':
+				parseCosine();
+			break;
+			case 't':
+				parseTangent();
+			break;
+			case 'h':
+				help();
+			break;
+			case 'e':
+				flag=false;
+			break;
+			default:
+				error();
+			break;
+		}
+	}
+	return 0;
+}
