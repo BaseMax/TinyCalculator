caefabd8f04a184c9a0595d05bd394a3187319cc
Max Base <MaxBaseCode@Gmail.Com>
2019-05-17 00:19:35 +0430

    Update and rename main.h to main.hpp

---
+++ main.hpp
@@ -0,0 +1,10 @@
+/**
+*
+* @Name : TinyCalculator/main.hpp
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-05-17
+* @Released under : https://github.com/BaseMax/TinyCalculator/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/TinyCalculator
+*
+**/
