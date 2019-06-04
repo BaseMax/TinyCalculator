97e5624d9f8cbcc20ac0368edb1da41e363c4c11
Max Base <MaxBaseCode@Gmail.Com>
2019-06-04 20:05:18 +0430

    Update main.hpp

--- main.hpp
+++ main.hpp
@@ -13,18 +13,26 @@
 
 	#include <iostream>
 	#include <cmath>
+	#include <inttypes.h>
+	
 	using namespace std;
 
-	void parseAddition();
-	void parseSubtraction();
-	void parseDivision();
-	void parseMultiplication();
-	void parseExponentiation();
-	void parseSquareRoot();
-	void parseSine();
-	void parseCosine();
-	void parseTangent();
-	void help();
-	void error();
+	void 	parseAddition();
+	void 	parseSubtraction();
+	void 	parseDivision();
+	void 	parseMultiplication();
+	void 	parseExponentiation();
+	void 	parseSquareRoot();
+	void 	parseSine();
+	void 	parseCosine();
+	void 	parseTangent();
+	void 	help();
+	void 	error();
+
+	float 	degree2Radian(float degree);
+	float 	implementationPower(float base, int power);
+	int64_t implementationFactorial(int number);
+	float 	implementationSine(float degree);
+	float 	implementationCosine(float degree);
 
 #endif
